bb8e8ab3fedaf019c01d06bbbb926497893c1c69
Steven Minor <steven.minor@gmail.com>
2024-10-21 11:14:07 -0700

    Removed stock photos and added real photos

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const importAll = (r) => {
   return r.keys().map(r);
 }
 
-const images = importAll(require.context('../assets/about_images', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../assets/about_images', false, /\.(png|jpe?g|svg|JPEG|JPG)$/));
 
 const About = () => {
 
